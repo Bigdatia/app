d411d61d7e0a1d15625b210cef62691d5452e60a
Alvarog0218 <122694377+Alvarog0218@users.noreply.github.com>
2026-01-08 11:32:28 -0500

    ....

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function ContactoPage() {
   // Reemplaza el número con tu número de WhatsApp
   // incluyendo el código de país (57 para Colombia), sin espacios ni símbolos
-  redirect('https://wa.me/573223765218');
+  redirect('https://wa.me/573164856744');
 
   // No es necesario retornar nada, la redirección interrumpe la renderización.
 }
