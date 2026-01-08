fe869a034ee4dcea2e17756947de129136437a2f
Alvarog0218 <122694377+Alvarog0218@users.noreply.github.com>
2026-01-08 11:38:57 -0500

    update mensaje wapp

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 export default function ContactoPage() {
   // Reemplaza el número con tu número de WhatsApp
   // incluyendo el código de país (57 para Colombia), sin espacios ni símbolos
-  redirect('https://wa.me/573164856744');
+  redirect("https://wa.me/573164856744?text=Hi,%20I'm%20interested%20in%20the%20service");
 
   // No es necesario retornar nada, la redirección interrumpe la renderización.
 }
